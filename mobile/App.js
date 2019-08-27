ae27f504bb49b2cfc98dc2d2816047969e1dff85
Waqas Ali <waqas.abbasi@outlook.com>
2019-08-27 16:56:59 +0800

    dev: switch heroku url

--- mobile/App.js
+++ mobile/App.js
@@ -1,22 +1,23 @@
-import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 
-import { Router } from 'react-native-router-flux';
-import { StatusBar } from 'react-native';
-import React from 'react';
+import { Router } from "react-native-router-flux";
+import { StatusBar } from "react-native";
+import React from "react";
 
-import { StyleProvider } from 'native-base';
-import getTheme from './native-base-theme/components';
-import theme from './native-base-theme/variables/commonColor';
+import { StyleProvider } from "native-base";
+import getTheme from "./native-base-theme/components";
+import theme from "./native-base-theme/variables/commonColor";
 
 import { Constants } from "expo";
 
-import Routes from './routes/index';
+import Routes from "./routes/index";
 const { manifest } = Constants;
 
-const graphQLURI = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev
-  ? `http://${manifest.debuggerHost.split(":").shift()}:4000/graphql`
-  : `https://quiet-sea-90084.herokuapp.com/graphql`;
+const graphQLURI =
+  typeof manifest.packagerOpts === `object` && manifest.packagerOpts.dev
+    ? `http://${manifest.debuggerHost.split(":").shift()}:4000/graphql`
+    : `https://hkuchisuncollege.herokuapp.com/graphql`;
 
 StatusBar.setHidden(false);
 
@@ -25,7 +26,7 @@ const client = new ApolloClient({
   onError: ({ networkError, graphQLErrors }) => {
     if (networkError) console.log(networkError);
     if (graphQLErrors) console.log(graphQLErrors);
-  },
+  }
 });
 
 const App = () => (
