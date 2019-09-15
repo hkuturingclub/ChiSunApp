3f1fe49d7b214e4a5f98af1fa5c0f3848d525ee4
Waqas Ali <waqas.abbasi@outlook.com>
2019-09-15 14:15:39 +0800

    Updates

---
+++ precache-manifest.ae4042796c6a9e8041849d819ffebedc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7734fd799059b4f6e43b954c0d57ea",
+    "url": "/ChiSunApp/index.html"
+  },
+  {
+    "revision": "de9dec824b6536873447",
+    "url": "/ChiSunApp/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "0b7e046ad3e72d4f34d0",
+    "url": "/ChiSunApp/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "de9dec824b6536873447",
+    "url": "/ChiSunApp/static/js/2.1a92002d.chunk.js"
+  },
+  {
+    "revision": "0b7e046ad3e72d4f34d0",
+    "url": "/ChiSunApp/static/js/main.24b98c21.chunk.js"
+  },
+  {
+    "revision": "829302cfd7250c7e3be1",
+    "url": "/ChiSunApp/static/js/runtime~main.24bf6ae2.js"
+  }
+]);
